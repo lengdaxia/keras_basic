c18146e5bc61fdbccd8ae5c2654633f5d1d1ea48
marlon <751081818@qq.com>
2018-07-02 21:39:32 +0800

    pooling layer -1

---
+++ Layers/pooling_layer.py
@@ -0,0 +1,8 @@
+from keras.layers import MaxPool1D,MaxPooling2D,MaxPooling3D
+
+# MaxPooling1D 层
+# 对时域1D信号进行最大值池化
+# keras.layers.pooling.MaxPooling1D(pool_size=2, strides=None, padding='valid')
+
+
+
